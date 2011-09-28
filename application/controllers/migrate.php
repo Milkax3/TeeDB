291ec25f93f3e327679f59c3947548486670279c
andreasgehle <andreas-gehle@web.de>
2011-09-28 21:10:30 +0200

    Added Migrate controller for setting migrations to current/latest and version
    Use root/migrate/current e.g.

---
+++ application/controllers/migrate.php
@@ -0,0 +1,80 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migrate extends CI_Controller {
+	
+	/**
+	 * Constructor
+	 */
+	function __construct()
+	{
+		parent::__construct();
+		
+		$this->load->library('migration');
+
+		/** VERY IMPORTANT - only turn this on when you need it. */
+		show_error('Access to this controller is blocked, turn me on when you need me.');
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Migrate current
+	 * 
+	 * This will migrate to the configed migration version
+	 */
+	public function current()
+	{
+		if ( ! $this->migration->current())
+		{
+			show_error($this->migration->error);
+			exit;
+		}
+
+		$this->output->append_output("<br />Migration Successful<br />");
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Migrate latest
+	 * 
+	 * This will migrate to the latest migration version
+	 */
+	public function latest()
+	{
+		if ( ! $this->migration->latest())
+		{
+			show_error($this->migration->error);
+			exit;
+		}
+
+		$this->output->append_output("<br />Migration Successful<br />");
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
+	 * Migrate to version
+	 * 
+	 * This will migrate up/down to the given migration version
+	 * 
+	 * @param int
+	 * @return void
+	 */
+	public function version($id = NULL)
+	{
+		// No $id supplied? Use the config version
+		$id OR $id = $this->config->item('migration_version');
+
+		if ( ! $this->migration->version($id))
+		{
+			show_error($this->migration->error);
+			exit;
+		}
+
+		$this->output->append_output("<br />Migration Successful<br />");
+	}
+}
+
+/* End of file migrate.php */
+/* Location: ./application/controllers/migrate.php */
