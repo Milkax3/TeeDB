6dc596238fef7bf42a56c1a1887f468324ccff3a
andreasgehle <andreas-gehle@web.de>
2012-02-09 22:52:33 +0100

    updated to h5bp 3, bootstrap 2, responsive

--- application/views/templates/default/footer.php
+++ application/views/templates/default/footer.php
@@ -1,31 +1,20 @@
-  <!-- JavaScript at the bottom for fast page loading -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo base_url('assets/js/libs/jquery-1.7.1.min.js'); ?>"><\/script>')</script>
 
-  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="assets/js/libs/jquery-1.6.4.min.js"><\/script>')</script>
+<!-- scripts concatenated and minified via ant build script-->
+<script src="<?php echo base_url('assets/js/libs/bootstrap/transition.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/libs/bootstrap/collapse.js'); ?>"></script>
 
+<script src="<?php echo base_url('assets/js/plugins.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/script.js'); ?>"></script>
+<!-- end scripts-->
 
-  <!-- scripts concatenated and minified via ant build script-->
-  <script defer src="assets/js/plugins.js"></script>
-  <script defer src="assets/js/script.js"></script>
-  <!-- end scripts-->
+<script>
+	var _gaq=[['_setAccount','<?php echo $google_analytic_id; ?>'],['_trackPageview']];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
 
-	
-  <!-- Change UA-XXXXX-X to be your site's ID -->
-  <script>
-    window._gaq = [['_setAccount','<?php echo $google_analytic_id; ?>'],['_trackPageview'],['_trackPageLoadTime']];
-    Modernizr.load({
-      load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
-    });
-  </script>
-
-
-  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
-       chromium.org/developers/how-tos/chrome-frame-getting-started -->
-  <!--[if lt IE 7 ]>
-    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-  <![endif]-->
-  
 </body>
 </html>
