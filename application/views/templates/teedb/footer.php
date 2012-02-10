ab54e7c92eab40383903c418d660a3216a45e93c
andreasgehle <andreas-gehle@web.de>
2012-02-10 14:21:00 +0100

    merge fix

--- application/views/templates/teedb/footer.php
+++ application/views/templates/teedb/footer.php
@@ -1,6 +1,6 @@
-	<?php if(!isset($large) OR !$large): ?>
+<?php if(!isset($large) OR !$large): ?>
 	</section><!-- Closing the #center section -->
-	
+		
 	<div class="image_border open">
 		<div class="center trans_border open"></div>
 	</div>
@@ -8,52 +8,39 @@
 	<div class="wrapper dark">
 		<div class="center transition min"></div>
 	</div>
-	<?php endif; ?>
-	
-	<div class="image_border close">
-		<div class="center trans_border close"></div>
-	</div>
+<?php endif; ?>
 
-	<footer id="main" class="wrapper light">
-		<p class="center">
-			TeeDB &copy; Copyright <?php echo date("Y"); ?> | Disclaimer All Rights Reserved | Privacy Policy - Page rendered in {elapsed_time} seconds
-		</p>
-	</footer>
+<div class="image_border close">
+	<div class="center trans_border close"></div>
+</div>
 
-	<!-- JavaScript at the bottom for fast page loading -->
+<footer id="main" class="wrapper light">
+	<p class="center" style="padding: 0">
+		TeeDB &copy; Copyright <?php echo date("Y"); ?> | Disclaimer All Rights Reserved | Privacy Policy - Page rendered in {elapsed_time} seconds
+	</p>
+</footer>
 
- 	<!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
- 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="assets/js/libs/jquery-1.7.min.js"><\/script>')</script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="<?php echo base_url('assets/js/libs/jquery-1.7.1.min.js'); ?>"><\/script>')</script>
 
+<!-- scripts concatenated and minified via ant build script-->
+<script src="<?php echo base_url('assets/js/libs/bootstrap/transition.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/libs/bootstrap/collapse.js'); ?>"></script>
 
- 	<!-- scripts concatenated and minified via ant build script-->
- 	<script defer src="assets/js/libs/jquery-ui-1.8.16.min.js"></script>
- 	<script defer src="assets/js/plugins.js"></script>
- 	<script defer src="assets/js/mylibs/jquery.adslider.js"></script>
- 	<script defer src="assets/js/mylibs/fileuploader.js"></script>
- 	<script defer src="assets/js/mylibs/jquery.form.js"></script>
-	<script defer src="assets/js/mylibs/jquery.nav.js"></script>
-  	<script defer src="assets/js/script.js"></script>
-  	<!-- end scripts-->
-  	
-	<?php if(isset($jquery) && $jquery) echo'<script>'.$jquery.'</script>'; ?>
-	
-	<!-- Change UA-XXXXX-X to be your site's ID -->
-	<script>
-		window._gaq = [['_setAccount','<?php echo $google_analytic_id; ?>'],['_trackPageview'],['_trackPageLoadTime']];
-	    Modernizr.load({
-	      load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
-	    });
-	</script>
+<script src="<?php echo base_url('assets/js/plugins.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/mylibs/jquery.adslider.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/mylibs/fileuploader.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/mylibs/jquery.form.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/mylibs/jquery.nav.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/script.js'); ?>"></script>
+<!-- end scripts-->
 
+<script>
+	var _gaq=[['_setAccount','<?php echo $google_analytic_id; ?>'],['_trackPageview']];
+	(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+	g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
 
-	<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
-		chromium.org/developers/how-tos/chrome-frame-getting-started -->
-	<!--[if lt IE 7 ]>
-		<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
-		<script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
-	<![endif]-->
-	
 </body>
 </html>
--- application/views/templates/teedb/header.php
+++ application/views/templates/teedb/header.php
@@ -1,44 +1,28 @@
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-  <meta charset="utf-8">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
-       More info: h5bp.com/b/378 -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title><?php echo (isset($title))? $title : 'Set title in config file.'; ?></title>
+	<meta name="description" content="<?php echo (isset($description))? $description : 'Set description in config file.'; ?>">
+	<meta name="author" content="<?php echo (isset($author))? $author : 'Set author in config file.'; ?>">
 
-  <title><?php echo (isset($title))? $title : 'Set title in config file.'; ?></title>
-  <meta name="description" content="<?php echo (isset($description))? $description : 'Set description in config file.'; ?>">
-  <meta name="author" content="<?php echo (isset($author))? $author : 'Set author in config file.'; ?>">
-  
-  <base href="<?php echo base_url(); ?>">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	
+	<base href="<?php echo base_url(); ?>">
 
-  <!-- Mobile viewport optimized: j.mp/bplateviewport -->
-  <meta name="viewport" content="width=device-width,initial-scale=1">
+	<link rel="alternate" type="application/rss+xml" href="<?php echo base_url('feed'); ?>" title="RSS feed for <?php echo (isset($title))? $title : 'Set title in config file.'; ?>">
+	
+	<link rel="shortcut icon" type="image/x-icon" href="<?php echo base_url('assets/favicon.ico'); ?>">
+	<link rel="apple-touch-icon" type="image/x-icon" href="<?php echo base_url('assets/apple-touch-icon.png'); ?>">
+	
+	<link rel="stylesheet" href='<?php echo base_url('assets/css/style.css'); ?>'>
 
-  <link rel="alternate" type="application/rss+xml" href="feed/" title="RSS feed for TeeDB">
-  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
-  <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico">
-  <link rel="apple-touch-icon" type="image/x-icon" href="assets/apple-touch-icon.png">
-
-  <!-- CSS: implied media=all -->
-  <!-- CSS concatenated and minified via ant build script-->
-  <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="assets/css/teedb.css">
-  <link rel="stylesheet" href="assets/css/jquery-ui-custom.css">
-  <!-- end CSS-->
-
-  <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
-
-  <!-- All JavaScript at the bottom, except for Modernizr / Respond.
-       Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries
-       For optimal performance, use a custom Modernizr build: www.modernizr.com/download/ -->
-  <script src="assets/js/libs/modernizr-2.0.6.min.js"></script>
+	<script src="<?php echo base_url('assets/js/libs/modernizr-2.5-respond-1.1.0.min.js'); ?>"></script>
 </head>
-
 <body class="light">
+<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
