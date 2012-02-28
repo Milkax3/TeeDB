723c3d54931a567acc9d92d1bf6d316b2ac3dc75
andreasgehle <andreas-gehle@web.de>
2012-02-28 14:08:39 +0100

    nicer dates function ( Create something like '4 minutes ago' )

--- application/helpers/MY_Date_helper.php
+++ application/helpers/MY_Date_helper.php
@@ -1,4 +1,52 @@
-<?php 
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if( ! function_exists('relative_time'))
+{
+    function relative_time($datetime)
+    {
+        $CI =& get_instance();
+        $CI->lang->load('date');
+        
+        if(!is_numeric($datetime))
+        {
+            $val = explode(" ",$datetime);
+           $date = explode("-",$val[0]);
+           $time = explode(":",$val[1]);
+           $datetime = mktime($time[0],$time[1],$time[2],$date[1],$date[2],$date[0]);
+        }
+        
+        $difference = time() - $datetime;
+        $periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+        $lengths = array("60","60","24","7","4.35","12","10");
+
+        if ($difference > 0) 
+        { 
+            $ending = $CI->lang->line('date_ago');
+        } 
+        else 
+        { 
+            $difference = -$difference;
+            $ending = $CI->lang->line('date_to_go');
+        }
+        for($j = 0; $difference >= $lengths[$j]; $j++)
+        {
+            $difference /= $lengths[$j];
+        } 
+        $difference = round($difference);
+        
+        if($difference != 1) 
+        { 
+            $period = strtolower($CI->lang->line('date_'.$periods[$j].'s'));
+        } else {
+            $period = strtolower($CI->lang->line('date_'.$periods[$j]));
+        }
+        
+        return "$difference $period $ending";
+    }
+} 
+
+
+
 if ( ! function_exists('datetime_to_human'))
 {
 	function datetime_to_human($datetime, $format="M j Y G:i"){
@@ -12,6 +60,8 @@ if ( ! function_exists('datetime_to_human'))
 			return date($format, $time);
 	}
 }
+
+
 
 if ( ! function_exists('timediff_to_now'))
 {
